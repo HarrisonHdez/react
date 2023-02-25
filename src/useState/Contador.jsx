16ce314df261594926ee829b89cd2be98fec9093
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-02-25 17:06:45 -0600

    practicando-react

---
+++ src/useState/Contador.jsx
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* Crea un componente de contador que muestre un número y tenga dos botones, uno para incrementar y otro para decrementar el número. Para implementar esto, puedes utilizar la función useState de React para mantener el estado del número y las funciones para manejar los eventos de los botones.
+
+// Recuerda que no te proporcionaré la solución, ¡así que tendrás que practicar y pensar en cómo implementar esto tú mismo! Pero aquí te dejo algunos pasos que podrías seguir:
+
+//* Crea un nuevo componente de React llamado "Contador".
+//* En el cuerpo del componente, utiliza la función useState de React para inicializar el estado del número en 0.
+//* Renderiza el número actual en la pantalla del componente.
+//* Agrega dos botones debajo del número, uno para incrementar y otro para decrementar.
+//* Agrega los manejadores de eventos para los botones que actualicen el estado del número utilizando la función setEstado de useState.
+//* Verifica que el contador se incremente y decremente correctamente al hacer clic en los botones.
+//* ¡Buena suerte practicando con useState en React!
