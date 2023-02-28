adee3d9b690537675c3007c49035b7adcd23b340
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-02-28 16:35:05 -0600

    practicando-react

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserList from './useEffect/UserList'
+import { Contador } from './useEffect/Contador'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserList />
+    <Contador />
   </React.StrictMode>,
 )
--- src/useEffect/Contador.jsx
+++ src/useEffect/Contador.jsx
@@ -1,31 +1,78 @@
 import React, { useEffect, useState } from 'react'
 
 export const Contador = () => {
-    const [count, setCount] = useState(110)
 
-    const increment = () => {
+    const [count, setCount] = useState(0)
+
+    const incrementCount = () => {
         setCount( count + 1 )
     }
 
     useEffect(() => {
-      document.title = `Contador ${ count }`
+      document.title = `Contador: ${ count }` 
+    
+      
     }, [count])
     
 
   return (
-        <>
-            <h2>{ count }</h2>
-            <button onClick={ increment }>+1</button>
-            
-        </>
-    )
+    <>
+       <h1>{ count }</h1>
+       <button onClick={ incrementCount }>+1</button>
+    </>
+  )
 }
+    
 
 
-//* Crea un componente de React que renderice un contador. El componente debe tener un estado interno llamado count que empieza en 0. También debe tener un botón que permita incrementar el contador en 1 cuando se hace clic en él.
 
-//* Además, utiliza useEffect para que el título de la página se actualice con el valor actual del contador cada vez que cambie.
 
-//* Recuerda que debes importar la función useEffect de React en tu componente y utilizarla dentro de tu componente.
 
-//* ¡Buena suerte practicando!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //* Crea un componente de React que renderice un contador. El componente debe tener un estado interno llamado count que empieza en 0. También debe tener un botón que permita incrementar el contador en 1 cuando se hace clic en él.
+
+// //* Además, utiliza useEffect para que el título de la página se actualice con el valor actual del contador cada vez que cambie.
+
+// //* Recuerda que debes importar la función useEffect de React en tu componente y utilizarla dentro de tu componente.
+
+// //* ¡Buena suerte practicando!
+
+
+
+// import React, { useEffect, useState } from 'react'
+
+// export const Contador = () => {
+//     const [count, setCount] = useState(110)
+
+//     const increment = () => {
+//         setCount( count + 1 )
+//     }
+
+//     useEffect(() => {
+//       document.title = `Contador ${ count }`
+//     }, [count])
+    
+
+//   return (
+//         <>
+//             <h2>{ count }</h2>
+//             <button onClick={ increment }>+1</button>
+            
+//         </>
+//     )
+// }
+
+
